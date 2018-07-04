f79906ebfa7538821a02f8bf2af8e3b4c0997f4a
Dennis Löhmann <>
2018-07-04 08:46:19 +0200

    Code improvements

--- src/lesson1/MainForMethodsWithUserEntry.java
+++ src/lesson1/MainForMethodsWithUserEntry.java
@@ -1,6 +1,6 @@
 package lesson1;
 
-public class MainForMethods {
+public class MainForMethodsWithUserEntry {
 	/** 
 	 * This is the main method
 	 */
--- src/lesson1/MainVariables.java
+++ src/lesson1/MainVariables.java
@@ -5,19 +5,23 @@ public class MainVariables {
 	 * This is the main method
 	 */
 	public static void main(String[] args) {
-		// My fancy program
+		// define a text variable
 		String hw = "Hello World!";
+		// define an integer number
 		int i = 12;
 		int j = 42;
-		double k = 12.0;
-		double m = 42.0;
+		// define a comma number (needs double memory space)
+		double k = 4000;
+		double m = 5;
 
+		// print it all out
 		System.out.println(hw); // 
 		System.out.println(k/m); // 
 		System.out.println(add(i,j)); // 
 
 	}
 	
+	// define an add function
 	public static int add(int i, int j) {
 		return i+j;
 	}
